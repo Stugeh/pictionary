24b0176c128cb5cff0926d14f06c531b43670b99
stuge <tuukkaveteli@gmail.com>
2021-04-17 16:20:30 +0300

    added username field to singleplayer settings

--- src/components/GameSetup/Settings.js
+++ src/components/GameSetup/Settings.js
@@ -1,15 +1,18 @@
 export const singlePlayerSettings = {
   easy: {
+    userName: '',
     columns: 5,
     rows: 3,
     tileTimer: 5,
   },
   medium: {
+    userName: '',
     columns: 8,
     rows: 5,
     tileTimer: 5,
   },
   hard: {
+    userName: '',
     columns: 10,
     rows: 8,
     tileTimer: 5,
