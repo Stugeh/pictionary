6ee9cd7bd6d60054a3d526f73f81745dc846afa9
stuge <tuukkaveteli@gmail.com>
2021-04-17 16:17:19 +0300

    added game setting objects

---
+++ src/components/GameSetup/Settings.js
@@ -0,0 +1,22 @@
+export const singlePlayerSettings = {
+  easy: {
+    columns: 5,
+    rows: 3,
+    tileTimer: 5,
+  },
+  medium: {
+    columns: 8,
+    rows: 5,
+    tileTimer: 5,
+  },
+  hard: {
+    columns: 10,
+    rows: 8,
+    tileTimer: 5,
+  },
+};
+
+export const MultiPlayerSettings = {
+  roundTimer: 30,
+  letterReveal: 8,
+};
