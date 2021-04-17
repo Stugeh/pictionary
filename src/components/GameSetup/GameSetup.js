2b62c4e4c18b276183ce9d5427608c7afa439e4d
stuge <tuukkaveteli@gmail.com>
2021-04-17 15:37:45 +0300

    added css grid and empty divs for all the grid fields

--- src/components/GameSetup/GameSetup.js
+++ src/components/GameSetup/GameSetup.js
@@ -5,13 +5,19 @@ import ModeToggle from '../ModeToggle';
 const GameSetup = ({setGameView, setActiveMode, activeMode}) => {
   return (
     <div className='gameSetup'>
-      <h1>
+      <h1 className='setupHeader'>
         {activeMode.toUpperCase()}
         <ModeToggle
           setActiveMode={setActiveMode}
           activeMode={activeMode}
         />
       </h1>
+      <div className='settings'>
+
+      </div>
+      <div className='playerList'>
+
+      </div>
     </div>
   );
 };
