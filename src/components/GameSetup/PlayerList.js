315b0b47002fdc525e356f81578a99726a4b026d
stuge <tuukkaveteli@gmail.com>
2021-05-04 18:39:46 +0300

    added and a placeholder text in the player list

--- src/components/GameSetup/PlayerList.js
+++ src/components/GameSetup/PlayerList.js
@@ -3,7 +3,9 @@ import Player from './Player';
 import {List} from '@material-ui/core';
 
 const PlayerList = ({playerList}) => (
-  <List>
+  <List className='playerList'>
+    <h2>Player List</h2>
+    {playerList.length < 1 ? <i>add some players</i> : null}
     {playerList.map((player) => (
       <Player key={player.name} player={player}/>
     ))}
