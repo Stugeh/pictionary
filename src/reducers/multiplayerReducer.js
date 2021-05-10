638a85da8e261c6566ca7a29d010ce17e5f52cc2
stuge <tuukkaveteli@gmail.com>
2021-05-10 22:14:53 +0300

    action and action creator for revealing a letter

--- src/reducers/multiplayerReducer.js
+++ src/reducers/multiplayerReducer.js
@@ -6,7 +6,9 @@ const reducer = (state = {}, action) => {
     case 'DECREMENT_TIMERS_MULTI':
       return {...state, timeLeft: action.data};
     case 'REVEAL_WORD_MULTI':
-      return {...state, visibleWord: currentWord.split('')};
+      return {...state, visibleWord: state.currentWord.split('')};
+    case 'REVEAL_LETTER_MULTI':
+      return {...state, visibleWord: action.data};
     case 'NEXT_ROUND_MULTI':
       const updatedState = {
         ...state,
@@ -23,6 +25,44 @@ const reducer = (state = {}, action) => {
 };
 
 /* Action creators */
+
+
+export const nextRound = () => {
+  dispatch({type: 'NEXT_ROUND_MULTI'});
+};
+
+export const decrementTimers = (round, letter) => {
+  const updatedTimers = {round: round-1, letter: letter-1};
+  dispatch({type: 'DECREMENT_TIMERS_MULTI', data: updatedTimers});
+};
+
+export const revealWord = () => {
+  dispatch({type: 'REVEAL_WORD_MULTI'});
+};
+
+export const revealLetter = (visible, word) => {
+  const availableIndexes = [];
+  const i = visible.indexOf('_');
+  while (i != -1) {
+    availableIndexes.push(i);
+    i = visible.indexOf('_', i + 1);
+  }
+  if (availableIndexes.length > 0) {
+    const randomIndex = Math.floor(Math.random() * availableIndexes.length);
+    const newVisible = [
+      ...visible,
+      visible[randomIndex] = word[randomIndex],
+    ];
+    dispatch({type: 'REVEAL_LETTER_MULTI', data: newVisible});
+  }
+};
+
+export const endRound = () => {
+
+};
+
+export const startRound = () => {
+};
 
 export const initGame = (settings) => {
   const newGame = {
@@ -43,31 +83,6 @@ export const initGame = (settings) => {
     },
   };
   dispatch({type: 'INIT_MULTI', data: newGame});
-};
-
-export const nextRound = () => {
-  dispatch({type: 'NEXT_ROUND_MULTI'});
-};
-
-export const decrementTimers = (round, letter) => {
-  const updatedTimers = {round: round-1, letter: letter-1};
-  dispatch({type: 'DECREMENT_TIMERS_MULTI', data: updatedTimers});
-};
-
-export const revealWord = () => {
-  dispatch({type: 'REVEAL_WORD_MULTI'});
-};
-
-export const revealLetter = () => {
-
-};
-
-export const endRound = () => {
-
-};
-
-export const startRound = () => {
-
 };
 
 export default reducer;
