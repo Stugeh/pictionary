ef434321d41303458389eeeb35f83549e59d562b
stuge <tuukkaveteli@gmail.com>
2021-05-12 14:55:54 +0300

    gameplay loop works. need to make timer ring and canvas reset after every round and add controls for pen size and color

--- src/components/GameView/popups/Winner.js
+++ src/components/GameView/popups/Winner.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const Winner = () => {
+import {Button} from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+import {nextRound} from '../../../reducers/multiplayerReducer';
+import {popupPreWordList} from '../../../reducers/popupReducer';
+
+const Winner = ({nextRound, popupPreWordList, winner, points}) => {
+  const handleNextRound = () => {
+    nextRound();
+    popupPreWordList();
+  };
   return (
     <div>
-
+      <h1>{winner} won that round and got +{points} points!</h1>
+      <Button
+        variant='contained'
+        onClick={handleNextRound}
+        style={{
+          width: '98%',
+          color: 'white',
+          backgroundColor: 'green',
+          fontSize: '20px',
+        }}
+      >
+        <h3>Next round</h3>
+        <NavigateNextIcon/>
+        <NavigateNextIcon/>
+      </Button>
     </div>
   );
 };
 
-export default Winner;
+const mapStateToProps = (state) => ({
+  winner: state.multiplayer.winner,
+  points: state.multiplayer.timeLeft.round * 100,
+});
+
+const mapDispatchToProps = {nextRound, popupPreWordList};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Winner);
