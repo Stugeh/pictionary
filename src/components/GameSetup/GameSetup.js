779974abceeecff0f228dcc21ae07efc0f7e9d8b
stuge <tuukkaveteli@gmail.com>
2021-05-13 13:39:28 +0300

    added a back button to setup view

--- src/components/GameSetup/GameSetup.js
+++ src/components/GameSetup/GameSetup.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+import {Button} from '@material-ui/core';
+
+import {switchViewMenu} from '../../reducers/menuReducer';
+
 import ModeToggle from '../ModeToggle';
 import SingleSettings from './SingleSettings';
 import MultiSettings from './MultiSettings';
 import PlayerList from './PlayerList';
 
 
-const GameSetup = ({activeMode}) => {
+const GameSetup = ({activeMode, switchViewMenu}) => {
   return (
     <div className='gameSetup'>
+      <Button
+        className='backButton'
+        variant='contained'
+        color='secondary'
+        onClick={switchViewMenu}
+      >
+        <KeyboardReturnIcon/>
+      </Button>
       <h1 className='setupHeader'>
         {activeMode.toUpperCase()}
         <ModeToggle />
@@ -27,4 +40,6 @@ const mapStateToProps = (state) => ({
   activeMode: state.menu.activeMode,
 });
 
-export default connect(mapStateToProps)(GameSetup);
+const mapDispatchToProps = {switchViewMenu};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameSetup);
