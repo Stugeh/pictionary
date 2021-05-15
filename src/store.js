670f2881a42c8e532004b0f13aae686afb88063d
stuge <tuukkaveteli@gmail.com>
2021-05-15 14:39:43 +0300

    added singleplayer reducer to the global state

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
 import multiplayerReducer from './reducers/multiplayerReducer';
+import singleplayerReducer from './reducers/singleplayerReducer';
 import popupReducer from './reducers/popupReducer';
 import menuReducer from './reducers/menuReducer';
 
 const reducer = combineReducers({
   multiplayer: multiplayerReducer,
+  singleplayer: singleplayerReducer,
   popup: popupReducer,
   menu: menuReducer,
 });
